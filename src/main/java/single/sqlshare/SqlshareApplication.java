48f613d69118a57521a518ba2e360e56ae3a1232
LooanCheong <jjyejun@naver.com>
2024-02-26 02:49:53 +0900

    Initial commit

---
+++ src/main/java/single/sqlshare/SqlshareApplication.java
@@ -0,0 +1,13 @@
+package single.sqlshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlshareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/single/sqlshare/SqlshareApplicationTests.java
@@ -0,0 +1,13 @@
+package single.sqlshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
