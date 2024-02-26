26f12ac8acefacdf2e87ac50f6edc3096b31c25f
LooanCheong <jjyejun@naver.com>
2024-02-26 17:52:53 +0900

    Clean: 게시글 서비스 import 정리

--- src/main/java/single/sqlshare/service/PostService.java
+++ src/main/java/single/sqlshare/service/PostService.java
@@ -1,7 +1,6 @@
 package single.sqlshare.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import single.sqlshare.domain.Post;
